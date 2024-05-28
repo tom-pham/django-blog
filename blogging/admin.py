b87feebc077cc2160f10cf789b6483c985271656
Tom Pham <thtupham@gmail.com>
2024-05-28 09:06:11 -0700

    Allow admin mode to designate category from a post within post admin

--- blogging/admin.py
+++ blogging/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 
 from blogging.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+    extra = 1
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CategoryInline]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ("posts",)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
