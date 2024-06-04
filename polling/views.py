49b86e2eb61c1c1c4fd973ab681011c85bc76682
Tom Pham <thtupham@gmail.com>
2024-06-03 22:15:11 -0700

    Create class based views for polling and replace with existing versions

--- polling/views.py
+++ polling/views.py
@@ -2,11 +2,46 @@ from django.shortcuts import render
 from polling.models import Poll
 from django.http import Http404
 
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+
+# class ListView:
+#     def as_view(self):
+#         return self.get
+
+#     def get(self, request):
+#         model_list_name = self.model.__name__.lower() + "_list"
+#         context = {model_list_name: self.model.objects.all()}
+#         return render(request, self.template_name, context)
+
+
+class PollListView(ListView):
+    model = Poll
+    template_name = "polling/list.html"
+
 
 # Create your views here.
 def list_view(request):
     context = {"polls": Poll.objects.all()}
     return render(request, "polling/list.html", context)
+
+
+class PollDetailView(DetailView):
+    model = Poll
+    template_name = "polling/detail.html"
+
+    def post(self, request, *args, **kwargs):
+        poll = self.get_object()
+
+        if request.POST.get("vote") == "Yes":
+            poll.score += 1
+        else:
+            poll.score -= 1
+        poll.save()
+
+        context = {"object": poll}
+        return render(request, self.template_name, context)
 
 
 def detail_view(request, poll_id):
