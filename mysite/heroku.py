9f60f0d98a30fe2fb6d1bcaa8d0956c13cca8549
Tom Pham <thtupham@gmail.com>
2024-06-18 08:24:07 -0700

    Add heroku.py

--- mysite/heroku.py
+++ mysite/heroku.py
@@ -2,3 +2,13 @@ import os
 import dj_database_url
 
 from .settings import *
+
+DATABASES = {"default": dj_database_url.config(default="sqlite://localhost")}
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+SECRET_KEY = os.environ["SECRET_KEY"]
+ALLOWED_HOSTS = ["*"]
+
+MIDDLEWARE = {"whitenoise.middleware.WhiteNoiseMiddleware", *MIDDLEWARE}
